0569b5483d9868de815ced81701ba4f296867b7c
umang <umangdhandhukiya97@gmail.com>
2025-09-26 11:22:42 +0530

    javascript variables

---
+++ 1_variables.js
@@ -0,0 +1,21 @@
+const accountId = 142312
+let accountEmail = "xya@gmail.com"
+var accountPass = "2122e"
+city = "rajkot"
+let accountState
+
+// accountId = 12 //not allowed
+
+accountEmail = "abc@gmail.com"
+accountPass = "2232"
+city = "ahemdabad"
+
+/*
+prefer not to use var
+because of issues in block scope and functional scope {scope}
+*/
+
+// console.log(accountId);
+// console.table([accountId,accountEmail,accountPass,city,accountState])
+
+console.log("hello world")
